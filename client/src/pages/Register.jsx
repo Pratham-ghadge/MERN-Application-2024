2a215f4f182591e897a07c3d9096c4be829bb3e1
Pratham-ghadge <prathamtech24@gmail.com>
2024-08-10 22:47:41 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -35,7 +35,7 @@ const Register = () => {
     
     e.preventDefault();
     console.log(user);
-     const response = await fetch(`http://localhost:5000/api/auth/register`,{
+     const response = await fetch(`https://mern-application-pratham.vercel.app/api/auth/register`,{
       method:'POST',
       headers:{
         "Content-Type": "application/json",

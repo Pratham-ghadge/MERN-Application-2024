f983418289cfa9b82e74c92c4f10dac678f1d2ca
Pratham-ghadge <prathamtech24@gmail.com>
2024-08-10 22:48:06 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
      const handleSubmit =  async (e) => {
       e.preventDefault();
       console.log(user);
-      const response = await fetch(`http://localhost:5000/api/auth/login`,{
+      const response = await fetch(`https://mern-application-pratham.vercel.app/api/auth/login`,{
         method:'POST',
         headers:{
           "Content-Type": "application/json",
